025b10832b0a77716ea50f5944add111de650a9d
aprizaaa <151981489+aprizaaa@users.noreply.github.com>
2024-02-23 21:49:07 +0700

    Add files via upload

---
+++ Form1.vb
@@ -0,0 +1,41 @@
+﻿Public Class Form1
+
+    Private Sub KeluarToolStripMenuItem_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles KeluarToolStripMenuItem.Click
+        End
+    End Sub
+
+
+    Sub terkunci()
+        LoginToolStripMenuItem.Enabled = True
+        LogoutToolStripMenuItem.Enabled = False
+        MasterToolStripMenuItem.Enabled = False
+        TransaksiToolStripMenuItem.Enabled = False
+        UtilityToolStripMenuItem.Enabled = False
+        LaporanToolStripMenuItem.Enabled = False
+
+    End Sub
+
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        Call terkunci()
+
+    End Sub
+
+    Private Sub LoginToolStripMenuItem_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles LoginToolStripMenuItem.Click
+        Form_Login.Show()
+
+    End Sub
+
+    Private Sub LogoutToolStripMenuItem_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles LogoutToolStripMenuItem.Click
+        Call terkunci()
+    End Sub
+
+    Private Sub PetugasToolStripMenuItem_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles PetugasToolStripMenuItem.Click
+        FormMaster_User.ShowDialog()
+    End Sub
+
+    Private Sub AnggotaToolStripMenuItem_Click(ByVal sender As System.Object, ByVal e As System.EventArgs)
+        FormMasterAnggota.Show()
+
+    End Sub
+End Class
---
+++ Form_Login.vb
@@ -0,0 +1,47 @@
+﻿Imports System.Data.Odbc
+Public Class Form_Login
+    Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button2.Click
+        Me.Close()
+
+    End Sub
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        Call koneksi()
+        cmd = New OdbcCommand("select * from user where id_user='" & TextBox1.Text & "' and password='" & TextBox2.Text & "'", conn)
+        dr = cmd.ExecuteReader
+        dr.Read()
+        If dr.HasRows Then
+            Me.Close()
+            Call bukakunci()
+        Else
+            MsgBox("ID User atau Password Salah!!!")
+
+        End If
+    End Sub
+
+    Sub bukakunci()
+        Form1.LoginToolStripMenuItem.Enabled = False
+        Form1.LogoutToolStripMenuItem.Enabled = True
+        Form1.MasterToolStripMenuItem.Enabled = True
+        Form1.TransaksiToolStripMenuItem.Enabled = True
+        Form1.UtilityToolStripMenuItem.Enabled = True
+        Form1.LaporanToolStripMenuItem.Enabled = True
+    End Sub
+
+    Private Sub Form_Login_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        TextBox1.Text = ""
+        TextBox2.Text = "123"
+        TextBox2.PasswordChar = "*"
+        Call koneksi()
+        lbltanggal.Text = Format(Today)
+
+
+
+
+    End Sub
+
+    Private Sub Timer1_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Timer1.Tick
+        lbljam.Text = Format(TimeOfDay)
+
+    End Sub
+End Class
